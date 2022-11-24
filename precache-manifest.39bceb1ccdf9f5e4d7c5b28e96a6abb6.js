0175d341075877aadc21f85bd86ef8984686e98f
Oratile Ditlhage <96663869+Exodera@users.noreply.github.com>
2022-11-24 12:57:37 +0200

    Updates

---
+++ precache-manifest.39bceb1ccdf9f5e4d7c5b28e96a6abb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa69896211f55b60ce6af942bfb6933f",
+    "url": "/Exodera/reviews/index.html"
+  },
+  {
+    "revision": "be9c35022244c1d3e2d5",
+    "url": "/Exodera/reviews/static/css/main.357267ea.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/Exodera/reviews/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Exodera/reviews/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be9c35022244c1d3e2d5",
+    "url": "/Exodera/reviews/static/js/main.298fe6c6.chunk.js"
+  },
+  {
+    "revision": "458810c910809e7d21a3",
+    "url": "/Exodera/reviews/static/js/runtime-main.7de40a4a.js"
+  }
+]);
